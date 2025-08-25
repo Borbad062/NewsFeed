60be7bd4b20a739e49d034e38bd87da9009261f5
Marvezi062 <Shohruhhekmatov007@gmail.com>
2025-08-25 13:18:31 +0700

    Fixed compose prod

--- api/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path, re_path
-
-urlpatterns = [
-    path('api/', include('app.urls')),
-    
-    path('api/v1/drf-auth/', include('rest_framework.urls')),
-    path('api/v1/auth/', include('djoser.urls')),
-    path('api/v1/auth/', include('djoser.urls.jwt')),
-]
